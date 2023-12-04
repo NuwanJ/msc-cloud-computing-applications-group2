58f55283d6d279f26395dfea7c7169efc1946d55
Nuwan Jaliyagoda <nuwan.jaliyagoda@rhino-partners.com>
2023-12-04 14:06:15 +0530

    Changes

--- src/handlers/SampleHandler.ts
+++ src/handlers/SampleHandler.ts
@@ -1,21 +1,33 @@
-import { IEventResult } from "../../types/APIGatewayTypes";
+import { IEventResult, RequestType } from "../../types/APIGatewayTypes";
 import { APIGatewayEventHandler } from "../lib/APIGatewayEventHandler";
 import { IEnvironmentProvider } from "../lib/EnvironmentProvider";
 import { EventResult } from "../lib/EventHandler";
 
 export class SampleHandler extends APIGatewayEventHandler {
+  // Path formats:
+  // - /sample
+  // - /sample/{action}
+
   async handle(): Promise<IEventResult> {
-    if (
-      this.event.requestContext.httpMethod === "GET" &&
-      this.getPathParam("action") == "action"
-    ) {
+    if (this.event.requestContext.httpMethod === RequestType.GET) {
       return new EventResult(
         {
-          message: "This is a sample message",
-          event: {
-            headers: this.getHeaders(),
+          message: "This is a sample message for a GET request",
+          request: {
             query: this.getQueryStringParameters(),
             path: this.getPathParameters(),
+          },
+        },
+        200
+      );
+    } else if (this.event.requestContext.httpMethod === RequestType.POST) {
+      return new EventResult(
+        {
+          message: "This is a sample message for a POST request",
+          request: {
+            query: this.getQueryStringParameters(),
+            path: this.getPathParameters(),
+            body: this.getBody(),
           },
         },
         200
--- types/APIGatewayTypes.ts
+++ types/APIGatewayTypes.ts
@@ -18,3 +18,11 @@ export enum Level {
   Warning = "WARNING",
   Error = "ERROR",
 }
+
+export enum RequestType {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  DELETE = "DELETE",
+  PATCH = "PATCH",
+}
