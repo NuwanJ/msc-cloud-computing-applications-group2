01bbab2c9fee2e73b5907a0c77f1b637730b08df
Nuwan Jaliyagoda <nuwan.jaliyagoda@rhino-partners.com>
2023-12-18 17:13:20 +0530

    Suggested

--- src/handlers/UserHandler.ts
+++ src/handlers/UserHandler.ts
@@ -4,28 +4,17 @@ import { APIGatewayEventHandler } from "../lib/APIGatewayEventHandler";
 import { IEnvironmentProvider } from "../lib/EnvironmentProvider";
 import { EventResult } from "../lib/EventHandler";
 import * as AWS from "aws-sdk";
-export class SampleHandler extends APIGatewayEventHandler {
-  // Path formats:
-  // - /sample
-  // - /sample/{action}
+
+export class UserHandler extends APIGatewayEventHandler {
   async handle(): Promise<IEventResult> {
     if (this.event.requestContext.httpMethod === RequestType.GET) {
-      return new EventResult(
-        {
-          message: "This is a sample message for a GET request",
-          request: {
-            query: this.getQueryStringParameters(),
-            path: this.getPathParameters(),
-          },
-        },
-        200
-      );
+      // Nothing for now
     } else if (this.event.requestContext.httpMethod === RequestType.POST) {
-      if (this.event.path == "/user") {
-        return await this.createUser();
+      if (this.getPathParam("action") == "register") {
+        return this.createUser();
+      } else if (this.getPathParam("action") == "login") {
+        return this.loginUser();
       }
-    } else {
-      return this.sampleFunction();
     }
 
     return new EventResult(null, 404);
@@ -33,6 +22,15 @@ export class SampleHandler extends APIGatewayEventHandler {
 
   async sampleFunction(): Promise<IEventResult> {
     return new EventResult({ Sample: "This is a sample function" }, 200);
+  }
+
+  async loginUser(): Promise<IEventResult> {
+    try {
+      return new EventResult({ message: "User login" }, 201);
+    } catch (error) {
+      console.error(error);
+      return new EventResult({ message: "Error login user" }, 500);
+    }
   }
 
   async createUser(): Promise<IEventResult> {
@@ -47,7 +45,8 @@ export class SampleHandler extends APIGatewayEventHandler {
     });
 
     const params = {
-      UserPoolId: "us-east-1_4PWxUyQI5",
+      UserPoolId: this.environmentProvider.getValue("USER_POOL_ID"), // to be moved into environment provider
+      // UserPoolId: process.env.USER_POOL_ID, // to be moved into environment provider
       Username: username,
       UserAttributes: [{ Name: "email", Value: email }],
       TemporaryPassword: password,
