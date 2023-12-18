8031477ead6571a90b01bad38babce59ae4db697
hasini_thilakarathna <hasinithilakarathna4@gmail.com>
2023-12-18 14:00:39 +0530

    Implement register API

--- src/handlers/SampleHandler.ts
+++ src/handlers/SampleHandler.ts
@@ -2,7 +2,7 @@ import { IEventResult, RequestType } from "../../types/APIGatewayTypes";
 import { APIGatewayEventHandler } from "../lib/APIGatewayEventHandler";
 import { IEnvironmentProvider } from "../lib/EnvironmentProvider";
 import { EventResult } from "../lib/EventHandler";
-
+import * as AWS from 'aws-sdk';
 export class SampleHandler extends APIGatewayEventHandler {
   // Path formats:
   // - /sample
@@ -20,17 +20,20 @@ export class SampleHandler extends APIGatewayEventHandler {
         200
       );
     } else if (this.event.requestContext.httpMethod === RequestType.POST) {
-      return new EventResult(
-        {
-          message: "This is a sample message for a POST request",
-          request: {
-            query: this.getQueryStringParameters(),
-            path: this.getPathParameters(),
-            body: this.getBody(),
-          },
-        },
-        200
-      );
+      // if(this.event.path == "/user"){
+        return this.createUser();
+      // }
+      // return new EventResult(
+      //   {
+      //     message: "This is a sample message for a POST request",
+      //     request: {
+      //       query: this.getQueryStringParameters(),
+      //       path: this.getPathParameters(),
+      //       body: this.getBody(),
+      //     },
+      //   },
+      //   200
+      // );
     } else {
       return this.sampleFunction();
     }
@@ -41,6 +44,49 @@ export class SampleHandler extends APIGatewayEventHandler {
   async sampleFunction(): Promise<IEventResult> {
     return new EventResult({ Sample: "This is a sample function" }, 200);
   }
+
+
+  async createUser(): Promise<IEventResult> {
+    if (this.event.requestContext.httpMethod !== RequestType.POST) {
+        return new EventResult({ message: "Method not allowed" }, 405);
+    }
+
+    const body = this.getBody();
+    const username  = body.username as string;
+    const email = body.email as string;
+    const password = body.password as string;
+    // const { username, email, password }  = body;
+
+    if (!username || !email || !password) {
+        return new EventResult({ message: "Missing required fields" }, 400);
+    }
+
+    const cognito = new AWS.CognitoIdentityServiceProvider({
+        region: 'us-east-1',
+    });
+
+    const params = {
+        UserPoolId: 'us-east-1_4PWxUyQI5',
+        Username: username,
+        UserAttributes: [
+            { Name: 'email', Value: email }
+        ],
+        TemporaryPassword: password,
+    };
+
+    try {
+        await cognito.adminCreateUser(params).promise();
+        return new EventResult({ message: "User created successfully" }, 201);
+    } catch (error) {
+        console.error(error);
+        return new EventResult({ message: "Error creating user" }, 500);
+    }
+}
+
+
+
+
+
 
   constructor(public environmentProvider: IEnvironmentProvider) {
     super(environmentProvider);
