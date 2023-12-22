7aecf2ff670fb4451336eb8913e3fdc20e9d15a4
hasara <hasarasamson97@gmail.com>
2023-12-22 12:35:11 +0530

    Changes to appointment-handler and yaml

--- src/appointment-handler.ts
+++ src/appointment-handler.ts
@@ -1,8 +1,15 @@
 import { AppointmentHandler } from "./handlers/AppointmentHandler";
 import { EnvironmentProvider } from "./lib/EnvironmentProvider";
+import { DynamoDBServiceProvider } from "./lib/DatabaseProvider";
 
 const environmentProvider = new EnvironmentProvider();
+const dynamodbProvider = new DynamoDBServiceProvider(
+    environmentProvider.getValue("AppointmentTable")
+);
 
-const handler = new AppointmentHandler(environmentProvider).getHandler();
+const handler = new AppointmentHandler(
+    environmentProvider,
+    dynamodbProvider
+).getHandler();
 
 export default handler;
